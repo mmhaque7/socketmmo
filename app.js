b2e30216e287051a36cee8d191a90d2f7695d8b3
Mehedi <mmhaque7@myseneca.ca>
2019-09-25 11:35:07 -0400

    fixed some node stuff

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+var HTTP_PORT = process.env.PORT || 8080;
 var express = require('express');
 var app = express();
-var serv = require('http').Server(app);
+
 
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/client/index.html');
 });
 app.use('/client', express.static(__dirname + '/client'));
 
-serv.listen(8080);
+serv.listen(HTTP_PORT);
 console.log('server connected');
 
 var SOCKET_LIST = {};
